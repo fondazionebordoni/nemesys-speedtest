0555581a63a7c848ceabef098fb013b25e8dcd0b
domenico.izzo@gmail.com <domenico.izzo@gmail.com@43301921-a9ec-3134-90cb-b189e72a36f9>
2012-10-31 10:18:06 +0000

    Commit to order files for packaging

--- nemesys/setup_win.py
+++ nemesys/setup_win.py
@@ -4,42 +4,40 @@ import sys, os
 from SysProf.windows import profiler
 from xml.etree import ElementTree as ET
 import modulefinder
+from glob import glob
+
+__version__ = '1.1.0'
+
+sys.path.append("C:\\Microsoft.VC90.CRT")
+
+data_files = [("Microsoft.VC90.CRT", glob(r'C:\Microsoft.VC90.CRT\*.*'))]
 
 profiler = profiler.Profiler()
 data = profiler.profile({'CPU'})
 print ET.tostring(data)
 
+
 class Target:
     def __init__(self, **kw):
         self.__dict__.update(kw)
         # for the versioninfo resources
-        self.version = "2.1.1"
+        self.version = __version__
         self.company_name = "Fondazione Ugo Bordoni"
         self.copyright = "(c)2011 Fondazione Ugo Bordoni"
-        self.name = "Nemesys"
-
-myservice = Target(
-    # used for the versioninfo resource
-    description = "Nemesys Service",
-    # what to build.  For a service, the module name (not the
-    # filename) must be specified!
-    modules = ['Nemesys'],
-    icon_resources = [(1, "..\\nemesys.ico")],
-    cmdline_style = 'pywin32',
-    )
+        self.name = "Nemesys Speedtest"
 
 setup(
+  data_files=data_files,
 	options = {
 		'py2exe': {
 			'packages': 'encodings',
       'optimize': 2,
 		}
 	},
-	name = 'Nemesys',
-	version = '2.1',
-	service = [myservice],
+	name = 'NemesysSpeedtest',
+	version = __version__,
 	windows = [
-		{"script": "gui.py", 'uac_info': "requireAdministrator", "icon_resources": [(1, "..\\nemesys.ico")]},
+		{"script": "NemesysSpeedtest.py", 'uac_info': "requireAdministrator", "icon_resources": [(1, "..\\nemesys.ico")]},
 	],
 	#packages = ['nemesys'],
 )
