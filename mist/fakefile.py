1da5615317b61ff29efca0bda18381fd1939ed91
ewedlund <ewedlund@users.noreply.github.com>
2015-05-07 14:50:14 +0200

    Missing initializations.

--- mist/fakefile.py
+++ mist/fakefile.py
@@ -21,7 +21,10 @@ import random
 class Fakefile:
 
   def __init__(self, bytes):
-    self._bytes = int(bytes)
+    self._initial_bytes = int(bytes)
+    self._bytes = self._initial_bytes
+    self.data = None
+    self.data_len = None
   
   def read(self, bufsize):
   
