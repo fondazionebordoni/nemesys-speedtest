b05ff3021284799097fb541ec1d0388df6693d83
ewedlund <ewedlund@users.noreply.github.com>
2016-04-07 14:27:43 +0200

    Removed copied lib, and more cleanup

--- mist/timeNtp.py
+++ mist/timeNtp.py
@@ -10,7 +10,7 @@
 #
 # This program is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.    See the
 # GNU General Public License for more details.
 #
 # You should have received a copy of the GNU General Public License
@@ -24,45 +24,33 @@ SERVERNTP = ["ntp.spadhausen.com","ntp.fub.it","time.windows.com","0.pool.ntp.or
 
 
 def _timestamp(server):
-  try:
     TimeRX = ntplib.NTPClient().request(server, version=3)
-  except Exception as e:
-    raise e
-  return TimeRX.tx_time
-  
+    return TimeRX.tx_time
+    
 def _ping(server):
-  try:
     delay = ping.do_one("%s" % server, 1) * 1000
-  except Exception as e:
-    raise e
-  return delay
-  
+    return delay
+    
 def timestampNtp():
-  local = True
-  for server in SERVERNTP:
-    delay = None
-    timestamp = None
-    try:
-      delay = _ping(server)
-      if (delay != None):
-        timestamp = _timestamp(server)
-        if (timestamp != None):
-          local = False
-          break
-    except Exception as e:
-      #logger.debug("Error: %s" % str(e))
-      pass
-      
-
-  if local:
-    server = 'this computer'
-    timestamp = time.time()
-      
-  #logger.debug("Time from %s: %s [%s]\n\n" % (server, datetime.fromtimestamp(timestamp).strftime('%Y/%m/%d %H:%M:%S'), timestamp))
-  return timestamp
+    local = True
+    for server in SERVERNTP:
+        delay = None
+        timestamp = None
+        try:
+            delay = _ping(server)
+            if (delay != None):
+                timestamp = _timestamp(server)
+                if (timestamp != None):
+                    local = False
+                    break
+        except Exception:
+            pass
+    if local:
+        server = 'this computer'
+        timestamp = time.time()
+    return timestamp
 
 if __name__ == '__main__':
-  request_num = 8
-  for x in range(request_num):
-#     logger.debug("Richiesta %s di %s:" % (x+1,request_num))
-    timestampNtp()
+    request_num = 8
+    for x in range(request_num):
+        print timestampNtp()
