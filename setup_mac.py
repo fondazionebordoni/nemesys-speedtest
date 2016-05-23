0fe2d99bb178068f84a27a28b7126eaa5fdd2ada
ewedlund <ewedlund@users.noreply.github.com>
2016-05-23 09:43:52 +0200

    App name

--- setup_mac.py
+++ setup_mac.py
@@ -26,7 +26,7 @@ def get_version():
 version = get_version()
 print "version: ", version
 APP = ['mist/mist.py']
-APP_NAME = 'MisuraInternetSpeedtest'
+APP_NAME = 'Mist'
 DATA_FILES = ['mist/mist.py']
 OPTIONS = {'argv_emulation': True,
 # 'plist': 'Info_mist.plist',
